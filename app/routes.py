3ce20ee4ff85b336be8be9d3eb17d2866064fc55
Bennie van Eeden <christiaanben@gmail.com>
2018-04-30 23:54:48 +0200

    renames

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,11 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
-def index():
+@app.route('/login')
+def login():
 	user = {'username': 'Dr Brink'}
-	return render_template('index.html')
+	return render_template('login.html')
+	
+@app.route('/register')
+def register():
+	return render_template('register.html')
