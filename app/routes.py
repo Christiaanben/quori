c153806e678fac1b5e978e3bc6a15f4ea16f037a
Bennie van Eeden <christiaanben@gmail.com>
2018-05-01 00:40:47 +0200

    added a bat file to run faster on windows

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,17 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/login')
 def login():
-	user = {'username': 'Dr Brink'}
-	return render_template('login.html')
+	form = LoginForm()
+	return render_template('login.html',form=form)
 	
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
 	return render_template('register.html')
+
+@app.route('/home')
+def home():
+	return render_template('home.html')
---
+++ launch.bat
@@ -0,0 +1,2 @@
+set FLASK_APP=quori.py
+flask run
