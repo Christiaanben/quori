6c8f8a211e79e2b16b2df466342ca3049630f2e1
Bennie van Eeden <christiaanben@gmail.com>
2018-05-01 03:18:45 +0200

    The login button now directs to home

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/login')
+@app.route('/',methods=['GET','POST'])
+@app.route('/login',methods=['GET','POST'])
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		#flash('Login requested for user {}'.format(form.username.data))
+		return redirect('/home')
 	return render_template('login.html',form=form)
 	
 @app.route('/register', methods=['GET','POST'])
